1911ab5d43eaf057b0d49b36cc23eca2fce0aa7e
Mark Connell <mark@connell.uk>
2025-10-17 16:25:05 +0100

    bundle gem human_colour

---
+++ lib/human_colour.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "human_colour/version"
+
+module HumanColour
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/human_colour/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module HumanColour
+  VERSION = "0.1.0"
+end
