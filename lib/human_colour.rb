e9328bd55beb6d6da4b5d001da54a0045ac35c70
Mark Connell <mark@connell.uk>
2025-10-17 16:44:50 +0100

    throw if we get a junk input

--- lib/human_colour.rb
+++ lib/human_colour.rb
@@ -6,6 +6,9 @@ module HumanColour
   class Error < StandardError; end
 
   def self.parse(rgb_string)
+    matches = rgb_string.match(/rgb\((\d+),\s*(\d+),\s*(\d+)\)/i)
+    raise Error if matches.nil?
+
     rgb_string == "rgb(255,255,255)" ? "white" : "black"
   end
 end
--- spec/human_colour_spec.rb
+++ spec/human_colour_spec.rb
@@ -10,5 +10,13 @@ RSpec.describe HumanColour do
       it { expect(described_class.parse("rgb(0,0,0)")).to eq("black") }
       it { expect(described_class.parse("rgb(255,255,255)")).to eq("white") }
     end
+
+    context "bad format" do
+      it "raises an exception" do
+        expect { described_class.parse("waffle") }.to(
+          raise_error { HumanColour::Error }
+        )
+      end
+    end
   end
 end
