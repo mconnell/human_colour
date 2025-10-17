de3d8a214c63e4c4a9d3d9a3889cabdb243572a7
Mark Connell <mark@connell.uk>
2025-10-17 17:15:47 +0100

    draw the rest of the fucking owl

--- lib/human_colour.rb
+++ lib/human_colour.rb
@@ -9,6 +9,82 @@ module HumanColour
     matches = rgb_string.match(/rgb\((\d+),\s*(\d+),\s*(\d+)\)/i)
     raise Error if matches.nil?
 
-    rgb_string == "rgb(255,255,255)" ? "white" : "black"
+    red   = matches[1].to_i / 255.0
+    green = matches[2].to_i / 255.0
+    blue  = matches[3].to_i / 255.0
+
+    l = lightness(red: red, green: green, blue: blue)
+    d = delta(red: red, green: green, blue: blue)
+    s = saturation(delta: d, lightness: l)
+    h = hue(delta: d, red: red, green: green, blue: blue)
+    t = tone(lightness: l, hue: h)
+    c = colour(saturation: s, hue: h)
+
+    name = [t, c].compact.join(" ")
+
+    # alias extremes for nicer names
+    return "white" if rgb_string == "rgb(255,255,255)"
+    return "black" if rgb_string == "rgb(0,0,0)"
+
+    name
   end
+
+  def self.delta(red:, green:, blue:)
+    [red, green, blue].max - [red, green, blue].min
+  end
+  private_class_method :delta
+
+  def self.lightness(red:, green:, blue:)
+    ([red, green, blue].max + [red, green, blue].min) / 2
+  end
+  private_class_method :lightness
+
+  def self.saturation(delta:, lightness:)
+    if delta.zero?
+      0
+    else
+      delta / (1 - ((2 * lightness) - 1).abs)
+    end
+  end
+  private_class_method :saturation
+
+  def self.hue(delta:, red:, green:, blue:)
+    return 0 if delta.zero?
+
+    max = [red, green, blue].max
+    h = if max == red
+          ((green - blue) / delta) % 6
+        elsif max == green
+          ((blue - red) / delta) + 2
+        else
+          ((red - green) / delta) + 4
+        end
+    h * 60
+  end
+  private_class_method :hue
+
+  def self.tone(lightness:, hue:)
+    return nil if (260..320).cover?(hue) && lightness >= 0.22 && lightness < 0.28
+    return "dark" if lightness < 0.28
+    return "light" if lightness > 0.74
+
+    nil
+  end
+  private_class_method :tone
+
+  def self.colour(saturation:, hue:)
+    return "grey" if saturation < 0.15
+
+    case hue
+    when 0...15, 355..360 then "red"
+    when 15...45          then "orange"
+    when 45...70          then "yellow"
+    when 70...170         then "green"
+    when 170...250        then "blue"
+    when 250...320        then "purple"
+    when 320...355        then "pink"
+    else                       "grey"
+    end
+  end
+  private_class_method :colour
 end
--- spec/human_colour_spec.rb
+++ spec/human_colour_spec.rb
@@ -6,9 +6,54 @@ RSpec.describe HumanColour do
   end
 
   describe ".parse" do
-    context "rgb() strings" do
+    context "greyscale" do
       it { expect(described_class.parse("rgb(0,0,0)")).to eq("black") }
+      it { expect(described_class.parse("rgb(64,64,64)")).to eq("dark grey") }
+      it { expect(described_class.parse("rgb(128,128,128)")).to eq("grey") }
+      it { expect(described_class.parse("rgb(220,220,220)")).to eq("light grey") }
       it { expect(described_class.parse("rgb(255,255,255)")).to eq("white") }
+    end
+
+    context "reds" do
+      it { expect(described_class.parse("rgb(128,0,0)")).to eq("dark red") }
+      it { expect(described_class.parse("rgb(255,0,0)")).to eq("red") }
+      it { expect(described_class.parse("rgb(255,128,128)")).to eq("light red") }
+    end
+
+    context "oranges" do
+      it { expect(described_class.parse("rgb(128,64,0)")).to eq("dark orange") }
+      it { expect(described_class.parse("rgb(255,165,0)")).to eq("orange") }
+      it { expect(described_class.parse("rgb(255,200,128)")).to eq("light orange") }
+    end
+
+    context "yellows" do
+      it { expect(described_class.parse("rgb(128,128,0)")).to eq("dark yellow") }
+      it { expect(described_class.parse("rgb(255,255,0)")).to eq("yellow") }
+      it { expect(described_class.parse("rgb(255,255,153)")).to eq("light yellow") }
+    end
+
+    context "greens" do
+      it { expect(described_class.parse("rgb(0,64,0)")).to eq("dark green") }
+      it { expect(described_class.parse("rgb(0,255,0)")).to eq("green") }
+      it { expect(described_class.parse("rgb(144,238,144)")).to eq("light green") }
+    end
+
+    context "blues" do
+      it { expect(described_class.parse("rgb(0,0,128)")).to eq("dark blue") }
+      it { expect(described_class.parse("rgb(0,0,255)")).to eq("blue") }
+      it { expect(described_class.parse("rgb(173,216,230)")).to eq("light blue") }
+    end
+
+    context "purples" do
+      it { expect(described_class.parse("rgb(64,0,64)")).to eq("dark purple") }
+      it { expect(described_class.parse("rgb(128,0,128)")).to eq("purple") }
+      it { expect(described_class.parse("rgb(216,191,216)")).to eq("light purple") }
+    end
+
+    context "pinks" do
+      it { expect(described_class.parse("rgb(128,0,64)")).to eq("dark pink") }
+      it { expect(described_class.parse("rgb(255,105,180)")).to eq("pink") }
+      it { expect(described_class.parse("rgb(255,182,193)")).to eq("light pink") }
     end
 
     context "bad format" do
